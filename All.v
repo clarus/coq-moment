6489775ed86a7d94c5c9ffb94cac7506adb5425e
Guillaume Claret <dev@clarus.me>
2014-10-22 11:47:24 +0200

    Conversions with Julian day

--- All.v
+++ All.v
@@ -1,8 +1,62 @@
-Require Import Coq.NArith.NArith.
+Require Import Coq.ZArith.ZArith.
 
-Local Open Scope N.
+Local Open Scope Z.
 
 Module Date.
+  Record t : Set := New {
+    year : Z;
+    month : Z;
+    day : Z }.
+
+  Definition compare (date1 date2 : t) : comparison :=
+    match Z.compare (year date1) (year date2) with
+    | Eq =>
+      match Z.compare (month date1) (month date2) with
+      | Eq => Z.compare (day date1) (day date2)
+      | c => c
+      end
+    | c => c
+    end.
+
+  Definition of_julian_day (is_gregorian : bool) (n : Z) : t :=
+    let a := n + 32044 in
+    let b := if is_gregorian then (4 * a + 3) / 146097 else 0 in
+    let c := if is_gregorian then a - (b * 146097) / 4 else n + 32082 in
+    let d := (4 * c + 3) / 1461 in
+    let e := c - (1461 * d) / 4 in
+    let m := (5 * e + 2) / 153 in
+    {|
+      day := e - (153 * m + 2) / 5 + 1;
+      month := m + 3 - 12 * (m / 10);
+      year := b * 100 + d - 4800 + m / 10 |}.
+
+  Definition to_julian_day (is_gregorian : bool) (date : t) : Z :=
+    let a := (14 - month date) / 12 in
+    let y := year date + 4800 - a in
+    let m := month date + 12 * a - 3 in
+    if is_gregorian then
+      day date + (153 * m + 2) / 5 + y * 365 + y / 4 - y / 100 + y / 400 - 32045
+    else
+      day date + (153 * m + 2) / 5 + y * 365 + y / 4 - 32083.
+
+  Compute of_julian_day true (to_julian_day true (New 2014 10 22)).
+  Compute to_julian_day false (New (-4712) 1 1).
+
+  Definition epoch : t := {|
+    year := 1970;
+    month := 1;
+    day := 1 |}.
+
+  Definition of_epoch_day (n : Z) : t :=
+    of_julian_day true (n + to_julian_day true epoch).
+
+  Definition to_epoch_day (date : t) : Z :=
+    to_julian_day true date - to_julian_day true epoch.
+
+  Compute of_epoch_day 16365.
+End Date.
+
+(*Module Date.
   (** The number of seconds since the Unix epoch. *)
   Record t : Set := New {
     open : N }.
@@ -24,4 +78,4 @@ Module Date.
 
   Definition is_pm (date : t) : bool :=
     negb (is_am date).
-End Date.
+End Date.*)
