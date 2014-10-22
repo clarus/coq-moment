70a844fc80a04ab04df248f6c792ca117f2b4e54
Guillaume Claret <dev@clarus.me>
2014-10-22 12:48:09 +0200

    Useless code removed

--- All.v
+++ All.v
@@ -101,27 +101,3 @@ Module Moment.
     Compute of_epoch (to_epoch now).
   End Test.
 End Moment.
-
-(*Module Date.
-  (** The number of seconds since the Unix epoch. *)
-  Record t : Set := New {
-    open : N }.
-
-  Definition seconds (date : t) : N :=
-    N.modulo (open date) 60.
-
-  Definition minutes (date : t) : N :=
-    N.modulo (open date / 60) 60.
-
-  Definition hours_12 (date : t) : N :=
-    N.modulo (open date / (60 * 60) - 1) 12 + 1.
-
-  Definition hours_24 (date : t) : N :=
-    N.modulo (minutes date) 24.
-
-  Definition is_am (date : t) : bool :=
-    N.ltb (hours_24 date) 12.
-
-  Definition is_pm (date : t) : bool :=
-    negb (is_am date).
-End Date.*)
